5ae260465cd500cbd24f5484485f86ea00e5a9a9
Dr.Code <stweb.it@gmail.com>
2017-08-08 22:09:37 +1000

    ДОбавил inputPassword

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText, placeholder }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 return (
   <View style={containerStyle}>
     <Text style={labelStyle}>{label}</Text>
     <TextInput
+      secureTextEntry={secureTextEntry}
       placeholder={placeholder}
       autoCorrect={false}
       style={inputStyle}
@@ -24,7 +25,7 @@ const styles = {
     paddingLeft: 5,
     fontSize: 18,
     lineHeight: 23,
-    flex: 4,
+    flex: 3,
   },
   labelStyle: {
     fontSize: 18,
