943c9d885ae34781175c42ea0beaa4a5b98b8122
Dr.Code <stweb.it@gmail.com>
2017-08-08 16:37:39 +1000

    инитиализировал базу в проекте

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyCKfhJ2bOYOJvHWKYUJFgsqGlhqkcPfAYo',
+    authDomain: 'auth-e82ae.firebaseapp.com',
+    databaseURL: 'https://auth-e82ae.firebaseio.com',
+    projectId: 'auth-e82ae',
+    storageBucket: 'auth-e82ae.appspot.com',
+    messagingSenderId: '845437101498'
+  });
+  }
+
   render() {
     return (
       <View>
